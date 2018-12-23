6554895c0e18cb5835727c3e847d868dc6bf61b1
Devon Letendre <Devonletendre@outlook.com>
2018-12-22 20:39:23 -0500

    Update deckofcards.py

--- deckofcards.py
+++ deckofcards.py
@@ -1,12 +1,12 @@
-#A superclass for a general set of cards
+# A superclass for a general set of cards
 from mydatastructures import ListNode
 from mydatastructures import DoublyLinkedList
 
-
 class DeckOfCards:
-	
-	#Takes either a string with cards separated by spaces, 
-	#or a list of strings, where each is a card.
+	'''
+	Takes either a string with cards separated by spaces, 
+	or a list of strings, where each is a card.
+	'''
 	def __init__(self, L = None):
 		self._L = DoublyLinkedList()
 		if L != None:
@@ -16,48 +16,51 @@ class DeckOfCards:
 				L = L.split()
 			for item in L:
 				self._L.addlast(item)
-	
-	#Removes and returns the card at the top of the deck.
+
 	def dealTop(self):
+		# Removes and returns the card at the top of the deck.
 		return self._L.removefirst()
 	
-	#Removes and returns the card at the bottom of the deck.
 	def dealBottom(self):
+		# Removes and returns the card at the bottom of the deck.
 		return self._L.removelast()
 	
-	#Adds card to the top of the deck.
 	def addTop(self, card):
+		# Adds card to the top of the deck.
 		self._L.addfirst(card)
 	
-	#Adds card to the bottom of the deck.
 	def addBottom(self, card):
+		# Adds card to the bottom of the deck.
 		self._L.addlast(card)
 	
-	#Adds pile to the top of the deck. The pile should itself 
-	#be a deck of cards. After this operation, the bottom card 
-	#of pile will be on top of the (formerly) top card of the deck.
 	def addPileTop(self, pile):
+		'''
+		Adds pile to the top of the deck. The pile should itself 
+		be a deck of cards. After this operation, the bottom card 
+		of pile will be on top of the (formerly) top card of the deck.
+		'''
 		pile._L += self._L
-		#self._L += pile._L
 		self._L = pile._L
 		pile._L = DoublyLinkedList()
-	
-	#Adds pile to the bottom of the deck. The pile should itself be 
-	#a deck of cards. After this operation, the top card of pile will 
-	#be below the (formerly) bottom card of the deck.
+		
 	def addPileBottom(self, pile):
+		'''
+		Adds pile to the bottom of the deck. The pile should itself be 
+		a deck of cards. After this operation, the top card of pile will 
+		be below the (formerly) bottom card of the deck.
+		'''
 		self._L += pile._L 
 	
-	#Deal out ncards cards to nplayers players. 
-	#It should return a list or a tuple of decks corresponding to the hands of the different players. 
-	#Dealing is assumed to follow the standard conventions that one card is given at a time, 
-	#and each new card given is added to the top of the hand that receives it.
 	def deal(self, nplayers, ncards = None):
+		'''
+		Deals out ncards cards to nplayers players. 
+		Returns a list or a tuple of decks corresponding to the hands of the different players. 
+		Dealing is assumed to follow the standard conventions that one card is given at a time, 
+		and each new card given is added to the top of the hand that receives it.
+		'''
 		PlayerRoster = []
-
 		for player in (range(nplayers)):
 			PlayerRoster.append(DeckOfCards())
-
 		if ncards == None:
 			while len(self._L) != 0:
 				for player in (range(nplayers)):
